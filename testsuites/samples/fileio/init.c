aec6b0f4d303b49da0ff145afdd31162224cbe5e
Nils Hölscher <nilhoel1@gmail.com>
2019-06-26 12:40:20 +0200

    altered fileio.c to test the TI drivers for pru

--- testsuites/samples/fileio/init.c
+++ testsuites/samples/fileio/init.c
@@ -33,6 +33,8 @@
 #include <rtems/nvdisk.h>
 #include <rtems/nvdisk-sram.h>
 #include <rtems/shell.h>
+#include <bsp/prussdrv.h>
+#include <bsp/pruss_intc_mapping.h>
 
 const char rtems_test_name[] = "FILE I/O";
 
@@ -215,6 +217,44 @@ shell_nvdisk_erase (int argc, char* argv[])
 
   return 0;
 }
+
+static int shell_pruss_loader(int argc, char *argv[]) {
+  if (argc != 2 && argc != 3) {
+    printf("Usage: %s loader text.bin [data.bin]\n", argv[0]);
+    return 1;
+  }
+
+  prussdrv_init();
+  if (prussdrv_open(PRU_EVTOUT_0) == -1) {
+    printf("prussdrv_open() failed\n");
+    return 1;
+  }
+
+  tpruss_intc_initdata pruss_intc_initdata = PRUSS_INTC_INITDATA;
+  prussdrv_pruintc_init(&pruss_intc_initdata);
+
+  printf("Executing program and waiting for termination\n");
+  if (argc == 3) {
+    if (prussdrv_load_datafile(0 /* PRU0 */, argv[2]) < 0) {
+      fprintf(stderr, "Error loading %s\n", argv[2]);
+      exit(-1);
+    }
+  }
+  if (prussdrv_exec_program(0 /* PRU0 */, argv[1]) < 0) {
+    fprintf(stderr, "Error loading %s\n", argv[1]);
+    exit(-1);
+  }
+
+  // Wait for the PRU to let us know it's done
+  prussdrv_pru_wait_event(PRU_EVTOUT_0);
+  printf("All done\n");
+
+  prussdrv_pru_disable(0 /* PRU0 */);
+  prussdrv_exit();
+
+  return 0;
+}
+
 
 static int
 shell_bdbuf_trace (int argc, char* argv[])
@@ -614,6 +654,8 @@ static void fileio_start_shell(void)
                        "bdbuf trace toggle", shell_bdbuf_trace);
   rtems_shell_add_cmd ("td", "files",
                        "Test disk", disk_test_block_sizes);
+  rtems_shell_add_cmd ("loader", "misc",
+                       "Test PRU", shell_pruss_loader);
 #if RTEMS_RFS_TRACE
   rtems_shell_add_cmd ("rfs", "files",
                        "RFS trace",
